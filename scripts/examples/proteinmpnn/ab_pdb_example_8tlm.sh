24960eb9c4e77cc27c16098c68b0f528d7631b9e
deepthought <deepthough@postech.ac.kr>
2025-03-17 17:50:18 +0900

    revert include/USalign

---
+++ scripts/examples/proteinmpnn/ab_pdb_example_8tlm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+poetry run python /home/scripts/proteinmpnn_interface_design.py \
+    -pdbdir /home/scripts/examples/rfdiffusion/example_outputs/8tlm \
+    -outpdbdir /home/scripts/examples/proteinmpnn/example_outputs/8tlm
---
+++ scripts/examples/rfdiffusion/antibody_pdbdesign_8tlm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+poetry run python  /home/scripts/rfdiffusion_inference.py \
+    --config-path /home/src/rfantibody/rfdiffusion/config/inference \
+    --config-name antibody \
+    antibody.target_pdb=/home/scripts/examples/rfdiffusion/example_inputs/CCR8_subset.pdb \
+    antibody.framework_pdb=/home/scripts/examples/rfdiffusion/example_inputs/hu-4D5-8_Fv.pdb \
+    inference.ckpt_override_path=/home/weights/RFdiffusion_Ab.pt \
+    'ppi.hotspot_res=[A175, V175, S176, E177, D178, G179, V180]' \
+    'antibody.design_loops=[H1:7,H2:6,H3:5-13]' \
+    inference.num_designs=100 \
+    inference.output_prefix=/home/scripts/examples/rfdiffusion/example_outputs/8tlm/ab_8tlm
